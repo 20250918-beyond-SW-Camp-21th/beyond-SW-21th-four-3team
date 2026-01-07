6af7dc61ee055c7cb51341e39153f40848fb5d6f
ehsy25 <eyoun17@gmail.com>
2026-01-07 16:45:59 +0900

    [chore] #1 프로젝트 생성

---
+++ todo-dodo/src/main/java/com/team3/todododo/TodoDodoApplication.java
@@ -0,0 +1,13 @@
+package com.team3.todododo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoDodoApplication.class, args);
+    }
+
+}
---
+++ todo-dodo/src/test/java/com/team3/todododo/TodoDodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.todododo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
