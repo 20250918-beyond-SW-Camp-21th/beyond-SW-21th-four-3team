268c1f6e1e93cadd4d65cb70034b57ac7d7986cb
danielhw99 <danielhw99@gmail.com>
2026-01-09 17:52:21 +0900

    [feat] #14 프론트엔드 로그인 및 Mockdata 삭제

--- todo-dodo-frontend/src/api/mockData.js
+++
@@ -1,7 +0,0 @@
-export const initialTodos = [
-    { id: 1, title: 'TODOLIST 1', status: 'Todo', date: new Date(2026, 0, 9), content: 'Detail 1' },
-    { id: 2, title: 'TODOLIST 2', status: 'In Progress', date: new Date(2026, 0, 9), content: 'Detail 2' },
-    { id: 3, title: 'TODOLIST 3', status: 'Done', date: new Date(2026, 0, 11), content: 'Detail 3' },
-    { id: 4, title: 'TODOLIST 4', status: 'Todo', date: new Date(2026, 0, 16), content: 'Detail 4' },
-    { id: 5, title: 'Work Meeting', status: 'Todo', date: new Date(2026, 0, 23), content: 'Project Sync' },
-];
